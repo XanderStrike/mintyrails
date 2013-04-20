0a6dae14175968d0df88da74c75a36361889839c
Alexander Standke <xanderstrike@gmail.com>
2013-04-19 20:27:28 -0700

    fix bugs

--- get-minty-rails.sh
+++ get-minty-rails.sh
@@ -15,21 +15,12 @@ cd ~
 echo -e "\e[01;32mInstalling prerequisites...\e[00m"
 
 pm="apt-get"
-sudo $pm -y install \
-    wget curl build-essential clang \
-    g++ vim \
-    bison openssl zlib1g \
-    libxslt1.1 libssl-dev libxslt1-dev \
-    libxml2 libffi-dev libyaml-dev \
-    libxslt-dev autoconf libc6-dev \
-    libreadline6-dev zlib1g-dev libcurl4-openssl-dev \
-    libtool >> $log_file 2>&1
-
-sudo $pm -y install libsqlite3-0 sqlite3 libsqlite3-dev mysql-client libmysqlclient-dev >> $log_file 2>&1
+sudo $pm -y install wget curl build-essential clang g++ vim bison openssl zlib1g libxslt1.1 libssl-dev libxslt1-dev libxml2 libffi-dev libyaml-dev libxslt-dev autoconf libc6-dev libreadline6-dev zlib1g-dev libcurl4-openssl-dev libtool
+sudo $pm -y install libsqlite3-0 sqlite3 libsqlite3-dev mysql-client libmysqlclient-dev
 
 
 echo -e "\e[01;32mInstalling Git...\e[00m"
-sudo $pm -y install git-core >> $log_file 2>&1
+sudo $pm -y install git-core
 
 
 echo -e "\e[01;32mInstalling Ruby+Rails via RVM...\e[00m"
@@ -37,12 +28,17 @@ curl -L https://get.rvm.io | bash -s stable --autolibs=3 --rails
 
 
 # Node.js
-
-echo -e "\e[01;32mInstalling node.js for javascript...\e[00m"
-git clone https://github.com/joyent/node.git .node
-cd .node
-git checkout v0.6.18 #Try checking nodejs.org for what the stable version is
-./configure && make && sudo make install
+echo -e "Install Node.js [Y/n]\c"
+read a
+if [[ $a == "N" || $a == "n" ]]; then
+	echo "Skipping Node.js"
+else
+	echo -e "\e[01;32mInstalling node.js...\e[00m"
+	git clone https://github.com/joyent/node.git .node
+	cd .node
+	git checkout v0.6.18 #Try checking nodejs.org for what the stable version is
+	./configure && make && sudo make install
+fi
 
 
 # Extras
