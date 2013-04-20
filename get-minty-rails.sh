3cd99a9b3754b307623078001c78ad51d4afe4fa
Alexander Standke <xanderstrike@gmail.com>
2013-04-19 20:15:40 -0700

    add imagemagick option

--- get-minty-rails.sh
+++ get-minty-rails.sh
@@ -17,7 +17,7 @@ echo -e "\e[01;32mInstalling prerequisites...\e[00m"
 pm="apt-get"
 sudo $pm -y install \
     wget curl build-essential clang \
-    g++ vim\
+    g++ vim \
     bison openssl zlib1g \
     libxslt1.1 libssl-dev libxslt1-dev \
     libxml2 libffi-dev libyaml-dev \
@@ -25,14 +25,18 @@ sudo $pm -y install \
     libreadline6-dev zlib1g-dev libcurl4-openssl-dev \
     libtool >> $log_file 2>&1
 
-sudo $pm -y install libsqlite3-0 sqlite3 libsqlite3-dev libmysqlclient-dev >> $log_file 2>&1
+sudo $pm -y install libsqlite3-0 sqlite3 libsqlite3-dev mysql-client libmysqlclient-dev >> $log_file 2>&1
 
 
 echo -e "\e[01;32mInstalling Git...\e[00m"
 sudo $pm -y install git-core >> $log_file 2>&1
 
+
 echo -e "\e[01;32mInstalling Ruby+Rails via RVM...\e[00m"
 curl -L https://get.rvm.io | bash -s stable --autolibs=3 --rails
+
+
+# Node.js
 
 echo -e "\e[01;32mInstalling node.js for javascript...\e[00m"
 git clone https://github.com/joyent/node.git .node
@@ -40,6 +44,17 @@ cd .node
 git checkout v0.6.18 #Try checking nodejs.org for what the stable version is
 ./configure && make && sudo make install
 
+
+# Extras
+
+echo -e "Install Imagemagick? [Y/n]\c"
+read a
+if [[ $a == "N" || $a == "n" ]]; then
+	echo "Skipping Imagemagick"
+else
+	echo -e "\e[01;32mInstalling Imagemagick.\e[00m"
+	sudo $pm -y install imagemagick libmagickcore-dev libmagickwand-dev
+fi
 
 echo -e "Generate SSH Keys? [Y/n]\c"
 read a
